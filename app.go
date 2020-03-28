d1912b258d2cce392dbec43e48e39cb907d454ab
cPluie <956787701@qq.com>
2020-03-28 20:14:18 +0800

    Update app.go

--- app.go
+++ app.go
@@ -9,7 +9,7 @@ func main(){
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("public"))))
 	http.Handle(server.SetDirToUrl, http.StripPrefix(server.SetDirToUrl, http.FileServer(http.Dir(server.SetDir))))
 
-	http.HandleFunc("/treefs"	, 	server.ScanDirTree)
+	http.HandleFunc("/treefs", 	server.ScanDirTree)
 	http.HandleFunc("/readfile", 	server.ReadFile)
 	http.HandleFunc("/writefile", 	server.WriteFile)	
 	http.HandleFunc("/getconfig", 	server.GetConfig)	
